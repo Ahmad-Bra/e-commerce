5637e052e5e9e345f788bbd7549ac75811ee63db
Ahmad Brazy <brazy@rzain.com>
2025-09-20 21:24:17 +0300

    fix problems

---
+++ dist/controlers/cart.js
@@ -0,0 +1,150 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Cart = void 0;
+const index_1 = require("../../generated/prisma/index");
+const redis_middleware_1 = require("../middleware/cashe/redis.middleware");
+const prisma = new index_1.PrismaClient();
+class Calculator {
+    calcTotalPrice(price, qty) {
+        return price * qty;
+    }
+    calcDiscount(prodPrice, discount, qty) {
+        const price = this.calcTotalPrice(prodPrice, qty);
+        const finalPrice = price - discount;
+        return Number(finalPrice.toFixed(2));
+    }
+}
+class Cart extends Calculator {
+    getAll(request, respones) {
+        return __awaiter(this, void 0, void 0, function* () {
+            // const { search } = request.query;
+            const { userId } = request.params;
+            try {
+                // if (search) {
+                //   const data = await prisma.cart.findMany({
+                //     where: {
+                //       OR: [
+                //         {
+                //           description: {
+                //             startsWith: String(search).trim(),
+                //             mode: "insensitive",
+                //           },
+                //         },
+                //         {
+                //           name: {
+                //             startsWith: String(search).trim(),
+                //             mode: "insensitive",
+                //           },
+                //         },
+                //       ],
+                //     },
+                //   });
+                //   respones.status(200).json(data);
+                //   return;
+                // }
+                const data = yield prisma.cart.findUnique({
+                    where: { userId },
+                    include: { items: { include: { product: true } } },
+                });
+                // set data to redis cache
+                redis_middleware_1.redisCacheMiddleware.setCache(request.originalUrl, data);
+                respones.status(200).json(data);
+                return;
+            }
+            catch (error) {
+                console.log(error);
+                respones.status(500).json({ messgae: error });
+                return;
+            }
+        });
+    }
+    create(request, respones) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { productId, quantity } = request.body;
+            const { userId } = request.params;
+            try {
+                let cart = yield prisma.cart.findUnique({ where: { userId } });
+                let product = yield prisma.products.findUnique({
+                    where: { id: productId },
+                });
+                if (!cart) {
+                    cart = yield prisma.cart.create({
+                        data: {
+                            userId,
+                            items: {
+                                create: [{ productId, quantity }],
+                            },
+                        },
+                    });
+                }
+                else {
+                    const existingItem = yield prisma.cartItem.findFirst({
+                        where: { cartId: cart.id, productId },
+                    });
+                    if (existingItem) {
+                        yield prisma.cartItem.update({
+                            where: { id: existingItem.id },
+                            data: { quantity: existingItem.quantity + quantity },
+                        });
+                    }
+                    else {
+                        yield prisma.cartItem.create({
+                            data: { cartId: cart.id, productId, quantity },
+                        });
+                    }
+                }
+                // const totalPrice = new Cart().calcTotalPrice(
+                //   foundProduct.price,
+                //   foundProduct.quantity
+                // );
+                // const priceWithDiscount = new Cart().calcDiscount(
+                //   foundProduct.price,
+                //   foundProduct.discount,
+                //   foundProduct.quantity
+                // );
+                respones
+                    .status(200)
+                    .json({ message: "product added to cart successfully" });
+                return;
+            }
+            catch (error) {
+                console.log(error);
+                respones.status(500).json({ messgae: error });
+                return;
+            }
+        });
+    }
+    delete(request, respones) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { userId } = request.params;
+            const { productId } = request.body;
+            try {
+                const cart = yield prisma.cart.findUnique({ where: { userId } });
+                if (!cart)
+                    return respones.status(404).json({ error: "Cart not found" });
+                yield prisma.cartItem.deleteMany({
+                    where: { cartId: cart.id, productId },
+                });
+                respones
+                    .status(200)
+                    .json({ message: "product removed from cart successfully" });
+                return;
+            }
+            catch (error) {
+                console.log(error);
+                respones.status(500).json({ messgae: error });
+                return;
+            }
+        });
+    }
+}
+exports.Cart = Cart;
---
+++ dist/stratgies/google.js
@@ -0,0 +1,62 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// @ts-nocheck
+const passport_google_oauth20_1 = require("passport-google-oauth20");
+const passport_1 = __importDefault(require("passport"));
+const index_js_1 = require("../../generated/prisma/index.js");
+const prisma = new index_js_1.PrismaClient();
+passport_1.default.serializeUser((user, done) => {
+    done(null, user.id);
+});
+passport_1.default.deserializeUser((id, done) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const findUser = yield prisma.google.findUnique({
+            where: { googleID: id },
+        });
+        findUser ? done(null, findUser) : done(null, null);
+    }
+    catch (error) {
+        console.log(error);
+        done(error, null);
+    }
+}));
+passport_1.default.use(new passport_google_oauth20_1.Strategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    scope: ["email", "profile"],
+    callbackURL: process.env.NODE_ENV == "development"
+        ? process.env.LOCALECALLBACKURL
+        : process.env.PRODCALLBACKURL,
+}, (accessToken, refreshTokenm, profile, done) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const findUser = yield prisma.google.findUnique({
+            where: { googleID: profile.id },
+        });
+        if (findUser)
+            return done(null, findUser);
+        const newUser = yield prisma.google.create({
+            data: {
+                username: profile.displayName,
+                googleID: profile.id,
+            },
+        });
+        if (newUser)
+            return done(null, newUser);
+    }
+    catch (error) {
+        console.log(error);
+        done(error, null);
+    }
+})));
